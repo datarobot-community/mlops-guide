b62e7a585157c2ca2fba947f549534d5688c476a
gonzo <gonzo@datarobot.com>
2020-06-30 10:54:21 -0600

    add consumer

--- automl-consumer/automl_predictions.py
+++ automl-consumer/automl_predictions.py
@@ -1,5 +1,7 @@
+import csv
 import json
 import os
+import signal
 from datetime import datetime
 from pathlib import Path
 from pprint import pformat
@@ -12,6 +14,11 @@ import requests
 from auto_mpg import Car
 from loguru import logger as log
 
+
+# def handle_sigterm(*args):
+#     raise KeyboardInterrupt()
+
+
 DATAROBOT_API_TOKEN = os.getenv("DATAROBOT_API_TOKEN")
 DATAROBOT_KEY = os.getenv("DATAROBOT_KEY")
 DATAROBOT_ENDPOINT = os.getenv("DATAROBOT_ENDPOINT")
@@ -21,7 +28,7 @@ now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 log.add(Path().cwd().joinpath("logs", f"automl_predictions-{now}.log"))
 
 
-def get_car_prediction(cars: List[Car]) -> Optional[Dict]:
+def get_car_prediction(cars: List[Car]) -> Optional[List]:
     prediction_headers = {
         "Authorization": f"Bearer {DATAROBOT_API_TOKEN}",
         "Content-Type": "application/json; charset=UTF-8",
@@ -42,17 +49,37 @@ def get_car_prediction(cars: List[Car]) -> Optional[Dict]:
         log.error(f"Something went wrong. Status: {response.status_code}")
         return None
     else:
-        prediction = response.json()
-        log.info(f"Predictions:\n{pformat(prediction)}")
-        return prediction
+        predictions = response.json()
+        log.info(f"Predictions:\n{pformat(predictions)}")
+        data = predictions["data"]
+        return [
+            [car["passthroughValues"]["car_id"], round(car["prediction"], 2)]
+            for car in data
+        ]
+
+
+class Shutdown:
+    kill_now = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        self.kill_now = True
 
 
 if __name__ == "__main__":
-    try:
-        while True:
-            some_cars = [Car() for _ in range(10)]
-            log.info("Making predictions.")
-            predictions = get_car_prediction(some_cars)
-            sleep(1)
-    except KeyboardInterrupt:
-        pass
+    shutdown = Shutdown()
+    predictions = []
+    while not shutdown.kill_now:
+        some_cars = [Car() for _ in range(10)]
+        log.info("Making predictions.")
+        summary = get_car_prediction(some_cars)
+        predictions.extend(summary)
+        sleep(1)
+    summary_file = Path().cwd().joinpath("logs", f"summary-{now}.csv")
+    with summary_file.open("w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["car_id", "mpg"])
+        writer.writerows(predictions)
